014a542f6b35cd9e9d7930e4b7ad18d48ba9e715
Arish Labroo <arishlabroo@hotmail.com>
2017-07-18 14:19:47 -0700

    Test with xunit autodata

--- ConceirgeTask/TaskServiceTest/UnitTest1.cs
+++ ConceirgeTask/TaskServiceTest/UnitTest1.cs
@@ -4,29 +4,66 @@ using Xunit;
 using Moq;
 using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit2;
+
 
 namespace TaskServiceTest
 {
+    /**
+     
+ ______   _______  ______      _________ _______  _______ _________ _______ 
+(  ___ \ (  ___  )(  __  \     \__   __/(  ____ \(  ____ \\__   __/(  ____ \
+| (   ) )| (   ) || (  \  )       ) (   | (    \/| (    \/   ) (   | (    \/
+| (__/ / | (___) || |   ) |       | |   | (__    | (_____    | |   | (_____ 
+|  __ (  |  ___  || |   | |       | |   |  __)   (_____  )   | |   (_____  )
+| (  \ \ | (   ) || |   ) |       | |   | (            ) |   | |         ) |
+| )___) )| )   ( || (__/  )       | |   | (____/\/\____) |   | |   /\____) |
+|/ \___/ |/     \|(______/        )_(   (_______/\_______)   )_(   \_______)
+                                                                            
+
+     */
+
+
     public class UnitTest1
     {
-        private readonly Mock<IAmADependency> _mockDependency;
         private readonly IFixture _fixture;
 
         public UnitTest1()
         {
             _fixture = new Fixture().Customize(new AutoMoqCustomization());
-            _mockDependency = _fixture.Freeze<Mock<IAmADependency>>();
         }
 
         [Fact]
         public async Task Test1()
         {
+            Mock<IAmADependency> _mockDependency = _fixture.Freeze<Mock<IAmADependency>>();
             _mockDependency.Setup(d => d.DoSomething(It.IsAny<string>())).ReturnsAsync((string a) => a.ToUpper());
             var sut = _fixture.Create<Sut>();
             var dto = _fixture.Create<RandomDto>();
             await sut.PrefixWithDance(dto);
-            Assert.StartsWith("Dance", dto.Frodo);
+            Assert.StartsWith("DANCE", dto.Frodo);
         }
+
+        [Theory, AutoMoqData]
+        public async Task Test2([Frozen] Mock<IAmADependency> deps, Sut sut, RandomDto dto)
+        {
+            var charlie = dto.Frodo;
+            deps.Setup(d => d.DoSomething(It.IsAny<string>())).ReturnsAsync("Apple");
+            await sut.PrefixWithDance(dto);
+            deps.Verify(d => d.DoSomething(charlie), Times.Once);
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute()
+            : base(new Fixture()
+                .Customize(new AutoMoqCustomization()))
+        {
+        }
+
     }
 
 
@@ -53,8 +90,7 @@ namespace TaskServiceTest
         public async Task PrefixWithDance(RandomDto dto)
         {
             var foo = await _dependency.DoSomething(dto.Frodo);
-            dto.Frodo = $"Dance{foo}";
+            dto.Frodo = $"DANCE{foo}";
         }
     }
 }
-
